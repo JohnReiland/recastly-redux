4d4e932307f73b7940eb2ed93cc38ca7dc075cd9
JohnReiland <reiland.shanghai@gmail.com>
2019-11-07 14:32:56 -0500

    "Preserve state"

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,11 +2,23 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
+import thunk from 'redux-thunk';
 
 
 var handleVideoSearch = (q) => {
-
   //TODO:  Write an asynchronous action to handle a video search!
+  var option = {
+    key: YOUTUBE_API_KEY,
+    query: q
+  }
+  return （dispatch） =>  {
+  searchYouTube(option, (videos) => {
+      return {
+      dispatch(changeVideoList(videos));
+      dispatch(changeVideo(videos[0]));
+      };
+    });
+  }
 };
 
 export default handleVideoSearch;
