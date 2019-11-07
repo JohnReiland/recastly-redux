42e5adb1847fef66623e804c084f746efe84c1f2
apple <apple@appletekiair.lan>
2019-11-07 01:31:22 -0800

    test cases 1 & 2 passed

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,9 +1,8 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
-  return {
-    type: 'CHANGE_VIDEO',
+
+    type: "CHANGE_VIDEO",
     video
-  };
 
 });
 
