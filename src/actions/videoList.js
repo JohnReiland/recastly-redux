5c0a63c16a4b8e40227cb66691d63fdf16b9ecef
apple <apple@appletekiair.lan>
2019-11-06 19:56:50 -0800

    just start redux

--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,10 @@
 var changeVideoList = (videos) => ({
   //TODO:  Return some action object to change the list of videos in VideoList.
+  return {
+    type: 'CHANGE_VIDEO_LIST',
+    videos
+  };
 });
 
 export default changeVideoList;
+
