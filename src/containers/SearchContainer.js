c192841807d01da57ba7d32e81a27c5f31ca6c9f
JohnReiland <reiland.shanghai@gmail.com>
2019-11-07 19:24:03 -0500

    "Save progress"

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,11 +1,14 @@
 import { connect } from 'react-redux';
 import Search from './../components/Search.js';
-import handleSearchChange from '../actions/search.js';
+import handleVideoSearch from '../actions/search.js';
 
 var SearchContainer = () => {};
 
-//TODO: define a SearchContainer component which will hook up your action
-// dispatchers with your search component props.
+//TODO: define a SearchContainer component
+//which will hook up your action dispatchers - handleVideoSearch
+// with your search component props.         - Search
+
+
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
 
--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,13 +1,13 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
   if (!state) {
     state = null;
   }
   switch (action.type) {
     case 'CHANGE_VIDEO':
-      return action.video;
+      return action.video || null;
     default:
       return state;
   }
